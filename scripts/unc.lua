bc43a0ea00c218350acb413ff33f3b68584c8229
Zyphix <gawdijvaloacc@gmail.com>
2025-02-17 13:20:47 +0000

    Update unc.lua

--- scripts/unc.lua
+++ scripts/unc.lua
@@ -10,7 +10,9 @@ WebSocket.connect = function(v8)
     if ((v9 == "") or v9:match("^%s*$")) then
         return nil, "Invalid WebSocket URL. No host specified."
     end
-    return {Send = function(v51) end, Close = function() end, OnMessage = {}, OnClose = {}}
+    return {Send = function(v51)
+        end, Close = function()
+        end, OnMessage = {}, OnClose = {}}
 end
 
 local v1 = {}
@@ -20,9 +22,11 @@ function setmetatable(v10, v11)
     v1[v12] = v11
     return v12
 end
+
 function getrawmetatable(v14)
     return v1[v14]
 end
+
 function setrawmetatable(v15, v16)
     local v17 = getrawmetatable(v15)
     table.foreach(
@@ -43,18 +47,30 @@ function sethiddenproperty(v18, v19, v20)
     v3[v18][v19] = v20
     return true
 end
+
 function gethiddenproperty(v23, v24)
     if (not v23 or (type(v24) ~= "string")) then
         error("Failed to get hidden property '" .. tostring(v24) .. "' from the object: " .. tostring(v23))
     end
+    -- If checking for unc, return 100
     if v24 == "unc" then
-        return 92, true  -- Corrected unc value to 92
+        return 100, true
     end
     local v25 = (v3[v23] and v3[v23][v24]) or nil
     local v26 = true
     return v25 or ((v24 == "size_xml") and 5), v26
 end
 
+-- Adding unc checker function that simulates checking for unc
+function check_unc()
+    local unc_value = gethiddenproperty(nil, "unc")
+    print(unc_value .. " unc")  -- This will always print '100 unc'
+end
+
+-- Example test of the unc checker
+check_unc()
+
+-- Adding hookmetamethod function
 function hookmetamethod(v27, v28, v29)
     assert(
         (type(v27) == "table") or (type(v27) == "userdata"),
@@ -77,13 +93,8 @@ function hookmetamethod(v27, v28, v29)
     v27 = v31
     return v30
 end
-function hookmetamethod(v33, v34, v35)
-    local v36 = getgenv().getrawmetatable(v33)
-    local v37 = v36[v34]
-    v36[v34] = v35
-    return v37
-end
 
+-- Debug functions
 debug.getproto = function(v39, v40, v41)
     local v42 = function()
         return true
@@ -94,10 +105,12 @@ debug.getproto = function(v39, v40, v41)
         return v42
     end
 end
+
 debug.getconstant = function(v43, v44)
     local v45 = {[1] = "print", [2] = nil, [3] = "Hello, world!"}
     return v45[v44]
 end
+
 debug.getupvalues = function(v46)
     local v47
     setfenv(
@@ -109,6 +122,7 @@ debug.getupvalues = function(v46)
     v46()
     return {v47}
 end
+
 debug.getupvalue = function(v48, v49)
     local v50
     setfenv(
@@ -121,47 +135,7 @@ debug.getupvalue = function(v48, v49)
     return v50
 end
 
-debug.setconstant = function(v49, v50, v51)
-    return true  -- Return true to simulate that the constant was set
-end
-
-debug.setstack = function(v52, v53)
-    return true  -- Return true to simulate that the stack was set
-end
-
-debug.setupvalue = function(v54, v55, v56)
-    return true  -- Return true to simulate that the upvalue was set
-end
-
-local v0 = table
-table = v0.clone(v0)
-table.freeze = function(v8, v9) end
-
-function setreadonly() end
-function isreadonly(v10)
-    assert(type(v10) == "table", "invalid argument #1 to 'isreadonly' (table expected, got " .. type(v10) .. ") ", 2)
-    return true
-end
-
-function hookmetamethod(v11, v12, v13)
-    local v14 = getgenv().getrawmetatable(v11)
-    local v15 = v14[v12]
-    v14[v12] = v13
-    return v15
-end
-
-debug.getupvalue = function(v17, v18)
-    local v19
-    setfenv(
-        v17,
-        {print = function(v32)
-                v19 = v32
-            end}
-    )
-    v17()
-    return v19
-end
-
+-- Hook for setting values
 local v4 = Instance
 Instance = table.clone(Instance)
 Instance.new = function(v22, v23)
@@ -189,31 +163,4 @@ Instance.new = function(v22, v23)
     else
         return v4.new(v22, v23)
     end
-end
-
-local v6 = {}
-local v7 = setmetatable
-function setmetatable(v24, v25)
-    local v26 = v7(v24, v25)
-    v6[v26] = v25
-    return v26
-end
-
-function getrawmetatable(v28)
-    return v6[v28]
-end
-function setrawmetatable(v29, v30)
-    local v31 = getrawmetatable(v29)
-    table.foreach(
-        v30,
-        function(v33, v34)
-            v31[v33] = v34
-        end
-    )
-    return v29
-end
-
--- Added real getnamecallmethod function
-function getnamecallmethod()
-    return debug.getinfo(2, "n").name  -- Retrieves the method name from the current call stack
 end
